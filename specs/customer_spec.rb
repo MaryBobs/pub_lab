a3baa6acb3646849ecfc511449ab7e9f75ea570c
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-10-23 10:43:34 +0100

    created customer setup

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -4,18 +4,19 @@ require_relative("../customer.rb")
 
 class TestCustomer < MiniTest::Test
 
+def setup
 
+@customer1 = Customer.new("Mary", 10)
+@customer2 = Customer.new("Adam", 15)
 
+end
 
+def test_customer_has_name
+  assert_equal("Mary", @customer1.name)
+end
 
-
-
-
-
-
-
-
-
-
+def test_wallet_has_cash
+  assert_equal(15, @customer2.wallet)
+end
 
 end
